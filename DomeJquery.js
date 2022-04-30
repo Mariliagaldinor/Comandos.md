3dd5b692bfc9db740ca711fcb9aa248a8002970d
Mariliagaldinor <101457472+Mariliagaldinor@users.noreply.github.com>
2022-04-30 11:39:02 -0400

    Create DomeJquery.js

---
+++ DomeJquery.js
@@ -0,0 +1,4 @@
+document.getElementById("title").innerHTML="Dom e Jquery";
+document.getElementsByTagName('button')[0].innerHTML="Nome";
+document.getElementsByTagName('li')[0].innerHTML="C";
+document.getElementsByTagName('button')[1].innerHTML="Inscreva-se";
